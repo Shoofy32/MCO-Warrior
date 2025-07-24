6aa2b8b53f58986888c00196336a84b641f9390a
Shoofy32 <stefandomingo17@gmail.com>
2025-07-24 15:16:36 +0800

    Javadoc (#16)
    * Delete Javadoc directory
    
    * Add files via upload
    
    * Delete MCO2 Java Files directory
    
    Replacing files with properly commented javadoc versions
    
    * Add files via upload
    
    * Update README.md (#15)

---
+++ MCO2-Files/Unique_Enemy.java
@@ -0,0 +1,95 @@
+/**
+ * Class file for the Unique Enemy class, this is a subclass of the abstract Enemy class
+ * <p>
+ *     Unique enemy is a bonus Enemy subclass.
+ * </p>
+ * <p>
+ *     Unique enemies are more powerful enemy types capable of using the bonus consumable, skill classes.
+ * </p>
+ * @see Enemy
+ */
+public class Unique_Enemy extends Enemy{
+    
+    private int counter = 0;
+    private String pattern;
+    char currentPatternTurn;
+
+    /**
+     * Constructor for the unique enemy class
+     * @param name name of the enemy, all names are assumed valid
+     * @param type type of the enemy, all types are assumed valid
+     * @param hitPoints base HP of the enemy, all values are assumed valid
+     * @param attack attack value of the enemy, all values are assumed valid
+     * @param defense defense value of the enemy, all values are assumed valid
+     * @param speed speed value of the enemy, all values are assumed valid
+     * @param pattern attack pattern of the enemy
+     * @param weapon weapon to be equipped by the enemy
+     * @param consumable consumable to be equipped by the enemy
+     */
+    public Unique_Enemy(String name, String type, int hitPoints, int attack, int defense, int speed, String pattern, Weapon weapon, Consumable consumable){
+
+        super(name, type, hitPoints, attack, defense, speed, pattern);
+        this.pattern = pattern;
+        totalEnemies++; //Increment
+        equipWeapon(weapon);
+        equipConsumable(consumable);
+
+    }
+
+    /**
+     * Concrete implementation of the <i>think</i> method
+     * Dictates what the enemy will do in a given turn
+     * @param target Character object targeted by the action
+     */
+    public void think(Character target){
+
+        switch(getTurnInputAction()){
+
+            case 'A':
+                
+                setLastCharacterAction("Attack");
+                attack(target);
+                break;
+
+            case 'D':
+
+                setLastCharacterAction("Defend");
+                defend();
+                break;
+
+            case 'C':
+
+                setLastCharacterAction("Charge");
+                charge();
+                break;
+
+            case 'U':
+
+                setLastCharacterAction("Consume");
+                setTurnInputAction(currentPatternTurn);
+                consume(target);          
+
+                break;
+
+            case 'F':
+
+                setLastCharacterAction("Skill");
+                useSkill(target);
+
+                break;
+            
+            default:
+                System.out.printf("\nCurrent choice does not exist within the given.\n");
+
+        }
+
+            
+        if(counter == pattern.length())
+            counter = 0;
+        else
+            counter++;
+
+    }
+
+
+}
